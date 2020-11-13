8054eada898a64c0a456f3b20d157cee3bc49cc5
jmjmjmjmjmjm <wlsauddlv@naver.com>
2020-11-13 17:40:01 +0900

    2020-11-13_17:40

---
+++ javastudy/src/ch02/Oper02.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class Oper02 {
+
+	public static void main(String[] args) {
+		int num =10;
+		num++; // 1씩 증가시켜주는 시퀀스
+		System.out.println(num);
+		
+		//num++ ==  num=num+1
+	}
+
+}
