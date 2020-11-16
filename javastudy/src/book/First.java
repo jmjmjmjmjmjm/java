6ec4d520ae41f4e16ae54ca4f01a6d5a97d7e2e9
jmjmjmjmjmjm <wlsauddlv@naver.com>
2020-11-16 17:39:16 +0900

    2020-11-16_17:40

---
+++ javastudy/src/book/First.java
@@ -0,0 +1,16 @@
+package book;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class First {
+	
+	public int kick;
+	public int punch;
+
+}
---
+++ javastudy/src/star1/Star2App.java
@@ -0,0 +1,14 @@
+package star1;
+
+public class Star2App {
+
+	public static void main(String[] args) {
+		Marine m = new Marine();
+		
+		Dragon d = new Dragon("µå¶ó±º1",100,20);
+		
+		
+		
+	}
+
+}
