85f84f845849ec9028e427a3d3f7c3c28a0d1d2f
jmjmjmjmjmjm <wlsauddlv@naver.com>
2020-11-24 17:47:51 +0900

    2020-11-24

---
+++ javastudy/src/test/progo.java
@@ -0,0 +1,15 @@
+//package test;
+//
+//
+//public class Bean {
+//	int id;
+//	
+//}
+//public class progo {
+//
+//	public static void main(String[] args) {
+//		System.out.println();
+//
+//	}
+//
+//}
